45c278d42015ed54e88907ac89660427c17536dd
0xc14m1z <0xc14m1z@gmail.com>
2018-11-19 13:42:57 +0100

    FIx a bug in FaIcon

--- src/base/FaIcon/FaIcon.js
+++ src/base/FaIcon/FaIcon.js
@@ -8,6 +8,7 @@ const LIGHT = 'fal'
 const REGULAR = 'far'
 const SOLID = 'fas'
 const BRAND = 'fab'
+const weights = { LIGHT, REGULAR, SOLID, BRAND }
 
 const XS = 'xs'
 const SM = 'sm'
@@ -17,6 +18,16 @@ const X3 = '3x'
 const X5 = '5x'
 const X7 = '7x'
 const X10 = '10x'
+const sizes = {
+  XS,
+  SM,
+  LG,
+  X2,
+  X3,
+  X5,
+  X7,
+  X10
+}
 
 export default class FaIcon extends PureComponent {
   static defaultProps = {
@@ -24,29 +35,14 @@ export default class FaIcon extends PureComponent {
   }
 
   static propTypes = {
-    weight: PropTypes.oneOf(Object.values(FaIcon.weights)),
+    weight: PropTypes.oneOf(Object.values(weights)),
     name: PropTypes.string.isRequired,
-    size: PropTypes.oneOf(Object.values(FaIcon.sizes)),
+    size: PropTypes.oneOf(Object.values(sizes)),
     className: PropTypes.string
   }
 
-  static weights = {
-    LIGHT,
-    REGULAR,
-    SOLID,
-    BRAND
-  }
-
-  static sizes = {
-    XS,
-    SM,
-    LG,
-    X2,
-    X3,
-    X5,
-    X7,
-    X10
-  }
+  static weights = weights
+  static sizes = sizes
 
   render() {
     const { weight, name, size, className } = this.props
