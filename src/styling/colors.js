66b72845d14da08e06c5a30d1e39474129c5dd40
0xc14m1z <0xc14m1z@gmail.com>
2018-11-26 16:40:26 +0100

    Add some color variables

---
+++ src/styling/colors.js
@@ -0,0 +1,7 @@
+export default {
+  red: '#CC1A36',
+  darkRed: '#B1172F',
+  redAccent: '#EE1032',
+
+  text: '#484848'
+}
--- src/styling/index.js
+++ src/styling/index.js
@@ -1,8 +1,10 @@
+import colors from './colors'
 import devices from './devices'
 import gutters from './gutters'
 import variables from './variables'
 
 export default {
+  colors,
   devices,
   gutters,
   variables
