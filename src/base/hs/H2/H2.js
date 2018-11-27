7afbe0bba13bae0271d00d24425ccb9912008a96
0xc14m1z <0xc14m1z@gmail.com>
2018-11-27 16:55:15 +0100

    Add H2 component

---
+++ src/base/hs/H2/H2.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+import styling from '../../../styling'
+
+const H2 = styled.h2`
+  font-family: ${styling.variables.font};
+  font-size: 32px;
+  font-weight: 600;
+  line-height: 1;
+  color: ${styling.colors.text};
+  margin: 0px;
+`
+
+export default H2
---
+++ src/base/hs/H2/H2.stories.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import H2 from './H2'
+
+storiesOf('Base/Hs/H2', module).add('standard', () => (
+  <H2>Lorem ipsum dolor sit amet</H2>
+))
