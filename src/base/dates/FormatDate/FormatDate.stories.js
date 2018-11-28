4106a0c7e95006c7b181b417ca170c6fe499514b
0xc14m1z <0xc14m1z@gmail.com>
2018-11-28 09:41:23 +0100

    Solve stories warnings

--- src/base/dates/FormatDate/FormatDate.stories.js
+++ src/base/dates/FormatDate/FormatDate.stories.js
@@ -4,8 +4,7 @@ import { storiesOf } from '@storybook/react'
 import FormatDate from './FormatDate'
 
 storiesOf('Base/dates/FormatDate', module)
-  .add('standard', () => <FormatDate />)
-  .add('with custom date', () => <FormatDate date={new Date(1989, 4, 22)} />)
+  .add('standard', () => <FormatDate date={new Date(1989, 4, 22)} />)
   .add('with custom format', () => (
-    <FormatDate format="MMMM Do YYYY, h:mm:ss a" />
+    <FormatDate date={new Date(1989, 4, 22)} format="MMMM Do YYYY, h:mm:ss a" />
   ))
